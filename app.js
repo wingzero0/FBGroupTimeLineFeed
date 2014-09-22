34c398b74ea6e56696da5951068d32c1549d5de0
kit at ubuntu <wingzzz2003@hotmail.com>
2014-09-22 16:30:39 +0800

    add fb login hook

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 	
 	var app = this.app || (this.app = {});
 	app.model = {};
-	
+
 }).call(this, jQuery);
 
 // view, must be init after "app.model" is ready
@@ -15,7 +15,17 @@
 
 // controller
 ;(function($){
-	
+	$("#setting").click(function(){
+		console.log("click setting");
+		FB.getLoginStatus(function(response) {
+			if (response.status === 'connected') {
+				console.log('Logged in.');
+			}
+			else {
+				FB.login();
+			}
+		});
+	});
 }).call(this, jQuery);
 
 
