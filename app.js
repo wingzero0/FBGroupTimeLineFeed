aa13c1a1957e964be9cf7f5d2404509920dead4b
kit at ubuntu <wingzzz2003@hotmail.com>
2014-09-22 15:42:38 +0800

    add template

---
+++ app.js
@@ -0,0 +1,21 @@
+// model
+;(function ($) {
+	
+	var app = this.app || (this.app = {});
+	app.model = {};
+	
+}).call(this, jQuery);
+
+// view, must be init after "app.model" is ready
+;(function($){
+	var app = this.app || (this.app = {});
+	app.view = {};
+	
+}).call(this, jQuery);
+
+// controller
+;(function($){
+	
+}).call(this, jQuery);
+
+
